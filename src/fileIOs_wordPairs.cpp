8108dd87062c697fe20a975edce206b129456319
Kaldaka <elliotjwarren@gmail.com>
2023-04-09 19:30:06 -0700

    started writing sentencesplitter() added splitter() helper method to cpp and h files. edited main cpp file for use with core methods. added .sln for use in vs added inputfile.txt for debugging

--- src/fileIOs_wordPairs.cpp
+++ src/fileIOs_wordPairs.cpp
@@ -1,9 +1,36 @@
 #include "fileIOs_wordPairs.h"
+#include <iostream>
+#include <fstream>
+#include <regex>
 
 namespace NS_WORDPAIRS{
 
     void sentenceSplitter(std::string& fname, std::vector<std::string>& sentences){
+        //TODO: split sentences based on ". or ? or ." or ?" or \n"
+        std::map<std::pair<std::string, std::string>, int> wordPairFreq_map;
+        std::string fileName = fname;
+        std::string line = "";
+        std::ifstream inFS;
+        std::cout << "opening " << fileName << "..." << std::endl;
 
+        inFS.open(fileName);
+        if (!inFS.is_open()) {
+            std::cout << "Could not open " << fileName << ", please try again." << std::endl;
+            std::cin >> fileName;
+            sentenceSplitter(fileName, sentences);
+        }
+
+        while (!inFS.fail()) {
+            std::getline(inFS, line);
+            sentences.push_back(splitter(line));
+        }
+        wordpairMapping(sentences, wordPairFreq_map);
+    }
+
+    std::string splitter(std::string& line) {
+        std::regex delim("[.?]\\n?|\"[.?]\"");
+        std::string loneSentence = "";
+        return loneSentence;
     }
 
     void wordpairMapping(std::vector<std::string>& sentences, std::map<std::pair<std::string,std::string>, int> &wordpairFreq_map){
--- src/fileIOs_wordPairs_main.cpp
+++ src/fileIOs_wordPairs_main.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 
 int main(){
+    std::string fileName = "";
+    std::vector<std::string> sentences;
 
-    std::cout << "hello world" << std::endl;
+    std::cout << "Please enter a filename you wish to analyze\n\n\tNote: please enter the file extension as well\n";
+    std::cin >> fileName;
+    
+    NS_WORDPAIRS::sentenceSplitter(fileName, sentences);
 
     //call functions from fileIOs_wordpairs.cpp
     //call unit tests
