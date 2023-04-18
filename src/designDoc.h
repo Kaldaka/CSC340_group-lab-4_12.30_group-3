6a77d86331f40e8a0174b7c55e34dcd98e69c9b2
Abdarrahman Ayyaz <35638978+AbdarrahmanAyyaz@users.noreply.github.com>
2023-04-18 02:02:46 -0700

    updated wordPairMapping design doc
    can be edited if needed

--- src/designDoc.h
+++ src/designDoc.h
@@ -20,5 +20,29 @@
 
     wordpairMapping()
         stuff
+        //Feel free to edit this if you think you can make it sound better for her requirements.
+        
+        
+        function wordpairMapping(const sentences: vector<string>, wordpairFreq_map: map<pair<string, string>, int>)
+    for each sentence in sentences do
+        tokens = empty vector of strings
+        tokenMaker(tokens, sentence) // tokenize the sentence
+         pairMaker(tokens, pairs) // make pairs
+        for i = 0 to tokens.size() - 2 then
+            for j = i + 1 to tokens.size() - 1 then
+                first = tokens[i]
+                second = tokens[j]
+                if (first > second) then // arrange the pair in lexicographical order
+                    swap(first, second)
+                pair = make_pair(first, second)
+                if (pair is not in seen_pairs) then // only count each pair once per sentence
+                    wordpairFreq_map[pair]++
+                    insert pair into seen_pairs
+                end of if
+            end of for
+        end of for
+    end of for
+end of function
+
 
 */
